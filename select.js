a9d8380170a1c40b450e3e025f9ff14cb184dcea
Mrlunsuper <irisslee95@gmail.com>
2020-02-01 15:26:14 +0700

    Add files via upload

--- select.js
+++ select.js
@@ -4,10 +4,10 @@ var all = [];
 selects.forEach(e=>{
    var options = Array.from(e.options);
    let coption = []
-   options.forEach(o=>{
+   options.forEach((o,index)=>{
     //    console.log(o.text,o.value,clean(o.getAttribute('data-rules')));
 
-    coption.push([o.text]);
+    coption.push({index:index,text:o.text,value:o.value,price:clean(o.getAttribute('data-rules'))});
       
    })
    all.push(coption);
@@ -34,11 +34,46 @@ function cartesian(arg) {
         }
     }
     helper([], 0);
-    return r.toString();
+    return r;
 }
-var data = [
-    [1,2],
-    [3,4,5],
-    [6,7]
-]
-console.log(cartesian(all));
+
+var master = cartesian(all);
+var allopt = document.getElementById('alloption');
+var strender = [];
+master.map((m,index)=>{
+   strender.push(mapText(m,index));
+})
+
+
+function mapText(m,index){
+    let obj = {
+        index:0,
+        text:'',
+        value:[],
+        id:[],price:0
+    }
+    
+    m.map((t)=>{
+        obj.index = index
+        obj.text+=t.text+' ';
+        obj.value.push(t.value);
+        obj.id.push(t.index);
+        obj.price += parseFloat(t.price);
+       
+    })
+    return obj;
+}
+
+strender.map(e=>{
+    let html='<div class="newoption">';
+    html+= e.text + '<p>'+e.price+'</p></div>';
+    $('#alloption').append(html);
+
+
+})
+
+$('#alloption ').on('click','.newoption',function(){
+
+    let index = ($(this).index());
+    console.log(strender[index]);
+});
